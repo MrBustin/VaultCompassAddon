590973f2e4d80d71ce7aa1407ce47ee07215ffca
Justin <redlegoboy1@gmail.com>
2025-05-20 16:36:03 -0400

    added Mixin

--- src/main/java/net/bustin/compass_distance/CompassDistance.java
+++ src/main/java/net/bustin/compass_distance/CompassDistance.java
@@ -1,21 +1,13 @@
 package net.bustin.compass_distance;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
+import org.spongepowered.asm.mixin.Mixins;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CompassDistance.MOD_ID)
@@ -32,6 +24,9 @@ public class CompassDistance
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        // Register your mixin config here
+        Mixins.addConfiguration("compass_distance.mixins.json");
     }
 
     private void setup(final FMLCommonSetupEvent event)
---
+++ src/main/java/net/bustin/compass_distance/mixin/PlayerMixin.java
@@ -0,0 +1,21 @@
+package net.bustin.compass_distance.mixin;
+
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.NbtComponent;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        System.out.println("Mixin working! Player tick method called.");
+    }
+}
