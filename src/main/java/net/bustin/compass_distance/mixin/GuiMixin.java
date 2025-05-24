faade1ca1632666d0fdda36a8e61c7b578869961
Justin <redlegoboy1@gmail.com>
2025-05-24 15:36:59 -0400

    moved display to above hot bar

--- src/main/java/net/bustin/compass_distance/mixin/GuiMixin.java
+++ src/main/java/net/bustin/compass_distance/mixin/GuiMixin.java
@@ -1,20 +1,28 @@
 package net.bustin.compass_distance.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import iskallia.vault.client.render.IVaultOptions;
+import iskallia.vault.skill.ability.effect.spi.core.Cooldown;
+import iskallia.vault.skill.ability.effect.spi.core.CooldownSkill;
+import iskallia.vault.util.CooldownGuiOption;
 import net.bustin.compass_distance.logic.VaultDirectionTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Optional;
+
+import static iskallia.vault.client.VaultAbilityKeyBinding.skill;
+
 
 @Mixin(Gui.class)
 public class GuiMixin {
+
 
     @Inject(method = "renderHotbar", at = @At("TAIL"))
     private void compassDistanceOverlay(float partialTicks, PoseStack poseStack, CallbackInfo ci) {
@@ -29,11 +37,12 @@ public class GuiMixin {
 
         int screenWidth = mc.getWindow().getGuiScaledWidth();
         int screenHeight = mc.getWindow().getGuiScaledHeight();
-
         Font font = mc.font;
-        int x = 10;
-        int y = screenHeight - 70;
 
+        int x = screenWidth/2 - font.width(text) / 2;
+        int y = screenHeight - 35;;
         font.draw(poseStack, text, x, y, 0xFFFFFF); // white text
+
+
     }
 }
