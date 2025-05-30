880c15fd3e74e009e8eb076ee9f6834022041975
Justin <redlegoboy1@gmail.com>
2025-05-30 13:52:11 -0400

    Added Placement Customizability using CTRL+Arrow Keys

--- src/main/java/net/bustin/vault_direction/logic/KeyInputHandler.java
+++ src/main/java/net/bustin/vault_direction/logic/KeyInputHandler.java
@@ -1,9 +1,17 @@
 package net.bustin.vault_direction.logic;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import iskallia.vault.gear.attribute.type.VaultGearAttributeTypeMerger;
+import iskallia.vault.gear.data.VaultGearData;
+import iskallia.vault.init.ModGearAttributes;
+import iskallia.vault.init.ModItems;
+import iskallia.vault.item.tool.HammerManager;
+import iskallia.vault.item.tool.IHammer;
 import net.bustin.vault_direction.VaultDirection;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,9 +26,22 @@ public class KeyInputHandler {
         if (event.phase != TickEvent.Phase.END) return;
 
         Minecraft mc = Minecraft.getInstance();
-        if (mc.screen != null || !mc.isWindowActive()) return;
-        if (mc.player == null) return;
 
+        if (mc.screen != null || !mc.isWindowActive()) return;
+        LocalPlayer player = mc.player;
+        if (player == null) return;
+
+        ItemStack stack = mc.player.getMainHandItem();
+
+        if (stack.getItem() == ModItems.TOOL) {
+            VaultGearData data = VaultGearData.read(stack);
+            boolean hasHammer = data.get(ModGearAttributes.HAMMERING, VaultGearAttributeTypeMerger.anyTrue());
+
+            if (hasHammer) {
+                // Player is holding a hammer-enabled Vault tool don't do keybind actions
+                return;
+            }
+        }
 
         if (Screen.hasControlDown()) {
             if (InputConstants.isKeyDown(mc.getWindow().getWindow(), GLFW.GLFW_KEY_UP)) {
